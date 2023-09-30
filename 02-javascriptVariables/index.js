7ca1d0facb7f1cdea430fcfce0ed8f58398e4a52
moshoodmohammed <mohammedamoshood@gmail.com>
2023-09-30 17:45:52 +0100

    test run for simple variable mastery

---
+++ 02-javascriptVariables/index.js
@@ -0,0 +1,9 @@
+var number1 = 5;
+number1  = 3;
+number1 = number1 + 2;
+const number2 = 15;
+var output = number2 / number1;
+
+var myString = 'Total Output ' + 'is: ';
+
+console.log(myString + output);
