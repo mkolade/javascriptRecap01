779f21d3f8854427b7d5df5d0b89c43530d44967
moshoodmohammed <mohammedamoshood@gmail.com>
2023-09-30 18:48:43 +0100

    done for now

--- 02-javascriptVariables/index.js
+++ 02-javascriptVariables/index.js
@@ -1,4 +1,5 @@
 //simple variable manipulation
+
 /* var number1 = 5;
 number1  = 3;
 number1 = number1 + 2;
@@ -6,7 +7,35 @@ const number2 = 15;
 var output = number2 / number1;
 
 var myString = 'Total Output ' + 'is: ';
+var notDefined;
 
+console.log(notDefined) //output is 'undefined'
 console.log(myString + output); */
 
 //arrays
+/* var numArray = ["One", "Two", "Three"];
+const myNumber = 2;
+
+console.log(numArray[myNumber]) */;
+
+//objects
+/* john = {
+    firstName: "John",
+    lastName: "Baptist",
+    Id: 77
+}
+billea = {
+    firstName: "Billea",
+    lastName: "Jean",
+    Id: 78
+}
+
+var kid = john.firstName + ' ' + billea.lastName;
+console.log(kid); */
+
+// use '===' when checking for null and typeof(null) = object;.use isNaN() to test for NaN which in turn stands for Not a Number;
+
+/* //Infinity
+console.log(1/0);//output is Infinity
+//Number constant usage
+Number.MAX_SAFE_INTEGER */
