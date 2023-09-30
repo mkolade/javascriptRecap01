a0da1cbbcdcf1a96ffe039b0efc4fd4d7143a1bd
moshoodmohammed <mohammedamoshood@gmail.com>
2023-09-30 17:48:08 +0100

    added commenting

--- 02-javascriptVariables/index.js
+++ 02-javascriptVariables/index.js
@@ -1,4 +1,5 @@
-var number1 = 5;
+//simple variable manipulation
+/* var number1 = 5;
 number1  = 3;
 number1 = number1 + 2;
 const number2 = 15;
@@ -6,4 +7,6 @@ var output = number2 / number1;
 
 var myString = 'Total Output ' + 'is: ';
 
-console.log(myString + output);
+console.log(myString + output); */
+
+//arrays
