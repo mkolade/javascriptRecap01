103ed1ef9689e4aebc1526334880448620485bfe
moshoodmohammed <mohammedamoshood@gmail.com>
2023-10-15 16:25:11 +0100

    shallow cloning

--- 05-objectManipulation/index.js
+++ 05-objectManipulation/index.js
@@ -1,1 +1,31 @@
 //OBJECTS
+
+//shallow cloning
+/* const someObj = {
+    0:"kd",
+    1:"ok",
+    2:"mk",
+    3:"md",
+    4:{
+        sake:"Japan"
+    },
+    length:5
+}
+
+const cloneObj = Object.assign({},someObj);
+//for deep cloning
+const deepClone = JSON.parse(JSON.stringify(someObj))
+//for obj to array
+const arrayObj = Array.from(someObj);
+
+someObj[3] = "notMd";
+
+console.log(cloneObj);// {0: 'kd', 1: 'ok', 2: 'mk', 3: 'md', 4: {…}, length: 5}
+console.log(arrayObj);// ['kd', 'ok', 'mk', 'md', {…}]
+console.log(deepClone);// {0: 'kd', 1: 'ok', 2: 'mk', 3: 'md', 4: {…}, length: 5}
+//iterating over an object
+for(var props in someObj){
+    if(someObj.hasOwnProperty(props)){
+        console.log(props);
+    }
+} */
