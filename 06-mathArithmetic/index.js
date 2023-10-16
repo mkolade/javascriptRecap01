a83c2a68c8d4b8e3cdd9d99dc179c0ad7e4912e2
moshoodmohammed <mohammedamoshood@gmail.com>
2023-10-16 16:08:30 +0100

    constants

--- 06-mathArithmetic/index.js
+++ 06-mathArithmetic/index.js
@@ -1,3 +1,6 @@
 //ARITHMETICS
 
 //constants
+//console.log(Math.PI,Math.E,Math.SQRT2)
+//console.log(Number.EPSILON,Number.MAX_VALUE,Number.MAX_SAFE_INTEGER,Number.MIN_VALUE,Number.MIN_SAFE_INTEGER)
+
