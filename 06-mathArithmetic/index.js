c304940feadbe8aae56261126ae48e809f3fbf29
moshoodmohammed <mohammedamoshood@gmail.com>
2023-10-16 16:39:04 +0100

    Remainder / Modulus (%)

--- 06-mathArithmetic/index.js
+++ 06-mathArithmetic/index.js
@@ -4,3 +4,15 @@
 //console.log(Math.PI,Math.E,Math.SQRT2)
 //console.log(Number.EPSILON,Number.MAX_VALUE,Number.MAX_SAFE_INTEGER,Number.MIN_VALUE,Number.MIN_SAFE_INTEGER)
 
+//Remainder / Modulus (%)
+/* console.log(42 % 13);//should be 2
+var fraction = 42 / 10;
+var fractionalPart = fraction % 1;
+console.log(fraction,fractionalPart);//should be 4.2 0.200018
+
+var n = 13;
+var i = 0;
+while (i = ((i + 1) % n)){
+    //console.log(i);
+    i++
+};//1 3 5 7 9 11 */
