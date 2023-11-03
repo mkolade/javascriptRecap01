801ea352f781ac347bc5868babdd1911041f3715
moshoodmohammed <mohammedamoshood@gmail.com>
2023-11-03 16:39:04 +0100

    //recursive && named functions

--- 07-functions/index.js
+++ 07-functions/index.js
@@ -16,7 +16,7 @@
 //console.log(a);//reference error
 foo();//Hello World! 12 */
 
-//currying
+/* //currying
 function foo( x, y, z){
     return x * y * z;
 }
@@ -32,4 +32,16 @@ console.log(prismVol(1)(2)(3));//6
 //IIFE=-Immediately invoked function Expressions
 (function(){
     console.log(46);
-}());
+}()); */
+
+//recursive && named functions
+var someSay = function sum(times){
+    if(times > 0){
+        console.log("Hello!");
+        sum(times - 1);
+    }
+}
+var saySomething = someSay;
+saySomething(2);//Hello! Hello!
+//sum(2);//reference error
+console.log(saySomething.name)//sum
