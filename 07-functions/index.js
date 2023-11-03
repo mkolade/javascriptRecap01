535c32500db5a419da46cd640e969586d97f4144
moshoodmohammed <mohammedamoshood@gmail.com>
2023-11-03 16:29:22 +0100

    IIFE=-Immediately invoked function Expressions

--- 07-functions/index.js
+++ 07-functions/index.js
@@ -1,11 +1,12 @@
 //FUNCTIONS
 
 //declaration
-function foo(){
-    const a = 11;
+/* function foo(){
+    const a = 'Hello';
     console.log(a);
     function foo2(){
         const b = 12;
+        const a = 'World!';
         console.log(a);
         console.log(b);
     }
@@ -13,4 +14,22 @@ function foo(){
     foo2();
 }
 //console.log(a);//reference error
-foo();//11 11 12
+foo();//Hello World! 12 */
+
+//currying
+function foo( x, y, z){
+    return x * y * z;
+}
+console.log(foo(1,2,3));//6
+function prismVol(l){
+    return function(w){
+        return function(h){
+            return l * w * h;
+        }
+    }
+}
+console.log(prismVol(1)(2)(3));//6
+//IIFE=-Immediately invoked function Expressions
+(function(){
+    console.log(46);
+}());
