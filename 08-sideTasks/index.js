d02af7bd1900318f279769095d13a25cbea82bff
moshoodmohammed <mohammedamoshood@gmail.com>
2023-11-05 15:59:20 +0100

    002 - Print the odd numbers less than 100

---
+++ 08-sideTasks/index.js
@@ -0,0 +1,11 @@
+//001 - Print numbers from 1 to 10
+for (var a = 1; a <= 10; a++){
+    //console.log(a);
+}
+
+//002 - Print the odd numbers less than 100
+for(var a = 0; a <= 100; a++){
+    if(a % 2 != 0){
+        console.log(a);
+    }
+}
