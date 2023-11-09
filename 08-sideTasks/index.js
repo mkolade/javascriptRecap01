920b55187abf58e5919c4eb6d52a57c2f3bb285c
moshoodmohammed <mohammedamoshood@gmail.com>
2023-11-09 15:38:15 +0100

    013-Find the maximum number in an array of numbers

--- 08-sideTasks/index.js
+++ 08-sideTasks/index.js
@@ -96,3 +96,14 @@ function findPosArray (array){
 findPosArray(myArray);
 
 //013 - Find the maximum number in an array of numbers
+var myArray = [1,2,3,4,5,6,7,8,9,10];
+function findMax(arr){
+    var max = arr[0]
+    for (var i = 0; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+};
+console.log(findMax(myArray));//10
