b71731d80948e43f2c79b73cffea254f9f83b2aa
moshoodmohammed <mohammedamoshood@gmail.com>
2023-11-10 22:12:13 +0100

    Static Methods

--- 09-forTheBigBoys01/index.js
+++ 09-forTheBigBoys01/index.js
@@ -1,32 +1,46 @@
 //CLASSES
 
 //declaration
-class speedyClass {
+class myFirstTest{
     constructor(option){
-        console.log(`This is created with ${option} option`);
+        console.log(`Hi this was made with the ${option} option`);
         this.option = option;
     };
 };
-const speedy = new speedyClass("speedy");//This is created with speedy option
-console.log(speedy.option);//speedy
+var someThing = new myFirstTest("speedy");//Hi this was made with the speedy option
+console.log(someThing.option);//speedy
 
 //inheritance
-class bigClass{
+class superClass{
     constructor(){
         this.logger = console.log;
-    }
+    };
     toLog(){
-        this.logger(`Hello class, this is ${this.name}`)
-    }
-}
-class smallClass extends bigClass{
-    constructor(){
-        super();
-        this.name = "Mike";
+        this.logger(`Hello, this is ${this.name}`);
     };
 };
 
-const newClass = new smallClass();
-newClass.toLog();
+class subClass extends superClass{
+    constructor(){
+        super();
+        this.name = "Sam";
+    };
+};
+var displayName = new subClass();
+displayName.toLog();//Hello, this is Sam
 
 //Static Methods
+class staticClass{
+    static myClassOps(){
+        console.log("Hi fucker!!");
+    };
+    static get myClassGet(){
+        console.log("You dnt need to call me with () hahaha!!.");
+    };
+}
+staticClass.myClassOps();//Hi fucker!!
+staticClass.myClassGet;//You dnt need to call me with () hahaha!!.
+class someNewClass extends staticClass{};
+var myNewClass = new staticClass();
+//myNewClass.myClassGet;//won't work
+someNewClass.myClassGet;//You dnt need to call me with () hahaha!!.
