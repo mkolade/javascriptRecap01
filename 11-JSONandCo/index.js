9f533597cfb68755c7155eb6f7e411be5383fac0
moshoodmohammed <mohammedamoshood@gmail.com>
2023-12-11 16:57:30 +0100

    manipulating JSON.stringify with replacer function

--- 11-JSONandCo/index.js
+++ 11-JSONandCo/index.js
@@ -23,13 +23,25 @@ changeBackground(myButton);
 
 //JSON
 
-//manipulating JSON
+    //manipulating JSON.stringify with replacer function
+        var myObject = [{ foundation: "Firefox", model: "browser", week: 42, transport: "bus", month: 17 },{ foundation: "Mozilla", model: "browser", week: 91, transport: "car", month: 3 }];
+        //normal function method
+            /* var jsonData = JSON.stringify(myObject, function (key,value){
+                if(typeof value === "string"){
+                    return;
+                }
+                return value;
+            });
+            console.log(JSON.parse(jsonData))//{week: 42, month: 87} */
 
-var jsonString = '[{"name":"Lionel","number":10},{"name":"Andres","number":8}]';
-//old method
-/* var data = JSON.parse(jsonString,function reviver(key,value){
-    return key === "name" ? value.toUpperCase()  : value
-}); */
-//new method
-var data = JSON.parse(jsonString,(key,value) => key === "name" ? value.toUpperCase : value);
-//console.log(data);
+        //other method
+            var jsonData = JSON.stringify(myObject,["foundation","model"])
+            console.log(JSON.parse(jsonData));
+    //manipulating JSON.parse with reviver function
+        //function method
+        /*  var normalData = JSON.parse(jsonData,function reviver(key,value){
+                return key === "foundation" ? value.toUpperCase()  : value
+            }); */
+        //arrow function method
+            var normalData = JSON.parse(jsonData,(key,value) => key === "foundation" ? value.toUpperCase() : value);
+            console.log(normalData);
